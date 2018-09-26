090c83c4051d6fcad0de73eaa55c6b3280fe0149
Joel Robert Justiawan <joelwindows7@gmail.com>
2018-09-26 11:18:47 +0700

    Fixed Broken Colliders
    Make sure to be careful when you try to import 2D gamekit. that kit changes things! damaging colliders e.g.!!!

---
+++ Assets/Scripts/FollowPlayerCSharp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//by Digital-Phantom
+//https://answers.unity.com/questions/930656/how-to-prevent-camera-rotation.html
+//Modification needed for 2D, so in this case, change Distance to 10.
+
+public class FollowPlayerCSharp : MonoBehaviour
+{
+    public Transform target; //This will be your citizen
+    public float distance;
+
+
+    void Update()
+    {
+        if (!target)
+        {
+            // Search for object with Player tag
+            var go = GameObject.FindWithTag("Player");
+            // Check we found an object with the player tag
+            if (go)
+                // Set the target to the object we found
+                target = go.transform;
+        }
+
+        if (target)
+            transform.position = new Vector3(target.position.x, target.position.y, target.position.z - distance);
+            //transform.position = new Vector3(target.position.x, target.position.y + 25, target.position.z - distance); // original. in 2D, Y must face right at it. So remove +25 on the Y vector.
+    }
+}
