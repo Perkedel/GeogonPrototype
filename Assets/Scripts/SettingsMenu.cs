295ed58e4156cd95f96dcc818fff0c8cd2632509
Joel Robert Justiawan <joelwindows7@gmail.com>
2018-09-30 22:56:31 +0700

    Learnt Brackey OptionMenu
    Despite the disasters mentioned previously (Ultimate Game UI is no longer free), We could handled it out with just a standard designs.
    .
    Now it is on ours to make our own graphics.

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+//by Brackey
+//Options menu!!!
+//optimised by Perkedel
+//Ultimate Game UI is not free!!! Please wait for the newer Gratis Open Full one in undefined time.
+//exclusive, on Hexagon Engine Asset store.
+//and less likely on Unity Asset store.
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityDropdown;
+
+    Resolution[] resolutions;
+    //QualityLevel[] qualityLevels;
+
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        //qualitySettings = QualitySettings.GetQualityLevel;
+        
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        List<string> optionq = new List<string>();
+
+        int currentResolutionIndex = 0;
+        int currentQualityIndex = 3;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+         }
+        //for(int i = 0; i < qualitySettings.Length; i++)
+        //{
+        //    string optiqn = qualitySettings[i].name;
+        //    optionq.Add(optiqn);
+
+        //    if(qualitySettings[i].name == QualitySettings.GetQualityLevel)
+        //    {
+
+        //    }
+        //}
+
+        //qualityDropdown.AddOptions(optionq);
+        qualityDropdown.value = currentQualityIndex;
+        qualityDropdown.RefreshShownValue();
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        //Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
