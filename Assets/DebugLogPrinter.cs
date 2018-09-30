5345512aa55b869c53372a264c491452b432a794
Joel Robert Justiawan <joelwindows7@gmail.com>
2018-09-30 21:00:06 +0700

    Ultimate Game UI is no longer free
    costs $25!! cancel idea! did not buy then.
    faak you who made it so!
    now Perkedel must create own. takes long. ready in undefined.

---
+++ Assets/DebugLogPrinter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLogPrinter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Ultimate Game UI is no longer Free!!!\nCosts $25!");
+        Debug.Log("Perkedel Can make own version!\nWhich may takes long time.");
+        Debug.Log("Sorry for inconveniences.\nPlease wait until it is ready in undefined time.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
