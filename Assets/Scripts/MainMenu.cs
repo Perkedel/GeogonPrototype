952f4f76d85ee71ffe01d326798bc3403affac5a
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-07-19 12:11:12 +0700

    What we're suppose to do with this?
    I think we should abandon this too to pursue Godot?

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     //Brackeys https://www.youtube.com/watch?v=zc8ac_qUXQY&t=3s
-
+    public bool isPauseGame = false;
+    [SerializeField] CanvasCore canvasCore;
+    //playScene
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        canvasCore.PlayTheLevel("SampleScene");
+    }
+    public void PlayGame(string passName)
+    {
+        //SceneManager.LoadScene(passName);
+        canvasCore.PlayTheLevel(passName);
     }
 
     public void QuitGame()
